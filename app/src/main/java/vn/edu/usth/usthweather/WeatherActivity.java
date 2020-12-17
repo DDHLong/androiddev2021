2ec972d2e1413eafd84a80fcab0acc797b6ea2e3
Đào Dương Hoàng Long <hoanglonghoab@gmail.com>
2020-12-17 21:07:05 +0700

    Add new fragment

--- app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import androidx.fragment.app.FragmentManager;
 
 public class WeatherActivity extends AppCompatActivity {
     private static final String TAG = WeatherActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.v(TAG, "In onCreate");
         setContentView(R.layout.activity_weather);
+
+        // Create a new fragment to be placed in the activity
+        ForecastFragment firstFragment = new ForecastFragment();
+
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
     }
 }
