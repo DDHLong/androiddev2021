2bdebd870278fabc4424ab8e0764fc03f582d1b5
Đào Dương Hoàng Long <hoanglonghoab@gmail.com>
2021-01-24 22:27:48 +0700

    Correcting path

--- app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
@@ -2,29 +2,72 @@ package vn.edu.usth.usthweather;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ForecastFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class ForecastFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ForecastFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ForecastFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ForecastFragment newInstance(String param1, String param2) {
+        ForecastFragment fragment = new ForecastFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(Color.parseColor("#fc5c9c"));
+//        view.setBackgroundColor(Color.parseColor("#f0f0ff"));
+
         LinearLayout llayout = (LinearLayout) view.findViewById(R.id.llayout);
 
         String[] dates = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
@@ -33,7 +76,7 @@ public class ForecastFragment extends Fragment {
             // Create LinearLayout
             LinearLayout row = new LinearLayout(this.getActivity());
             LinearLayout.LayoutParams row_params = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT, 0, 1f);
+                    LinearLayout.LayoutParams.MATCH_PARENT, 200, 0);
             row_params.setMargins(0, 10, 0, 10);
             row.setLayoutParams(row_params);
             row.setOrientation(LinearLayout.HORIZONTAL);
@@ -51,8 +94,8 @@ public class ForecastFragment extends Fragment {
             row.addView(dText);
 
             // Random status
-            int[] icons = {R.drawable.sun, R.drawable.sunset, R.drawable.night, R.drawable.cloudy};
-            String[] stt = {"Sunny", "Sunset", "Night", "Cloudy"};
+            int[] icons = {R.drawable.sun, R.drawable.cloudy, R.drawable.elipse, R.drawable.night};
+            String[] stt = {"Sunny", "Windy", "Rainy", "Cloudy"};
             int n = (int)(Math.random()*4);
 
             // Icons
@@ -93,6 +136,5 @@ public class ForecastFragment extends Fragment {
 
         return view;
     }
+
 }
-
-
--- app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -1,22 +1,64 @@
 package vn.edu.usth.usthweather;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.util.Log;
 import androidx.fragment.app.FragmentManager;
 
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.Toast; // Use Toast class to display message
+
+import android.os.Bundle;
+
+import vn.edu.usth.usthweather.WeatherFragment;
+
 public class WeatherActivity extends AppCompatActivity {
-    private static final String TAG = WeatherActivity.class.getSimpleName();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
         // Create a new fragment to be placed in the activity
-        WeatherFragment firstFragment = new WeatherFragment();
-        ForecastFragment secondFragment = new ForecastFragment();
+        ForecastFragment firstFragment = new ForecastFragment();
+        WeatherFragment secondFragment = new WeatherFragment();
 
         // Add the fragment to the 'container' FrameLayout
+//        getSupportFragmentManager().beginTransaction().add(R.id.container, secondFragment).commit();
+//        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+//        Toast.makeText(getApplicationContext(),"Now onStart() calls", Toast.LENGTH_LONG).show(); //onStart Called
+        Log.i("WeatherActivity", "onStart"); // Send an INFO log message
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+//        Toast.makeText(getApplicationContext(),"Now onResume() calls", Toast.LENGTH_LONG).show();
+        Log.i("WeatherActivity", "onResume"); // Send an INFO log message
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+//        Toast.makeText(getApplicationContext(),"Now onPause() calls", Toast.LENGTH_LONG).show();
+        Log.i("WeatherActivity", "onPause"); // Send an INFO log message
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+//        Toast.makeText(getApplicationContext(),"Now onStop() calls", Toast.LENGTH_LONG).show();
+        Log.i("WeatherActivity", "onStop"); // Send an INFO log message
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+//        Toast.makeText(getApplicationContext(),"Now onDestroy() calls", Toast.LENGTH_LONG).show();
+        Log.i("WeatherActivity", "onDestroy"); // Send an INFO log message
     }
 }
